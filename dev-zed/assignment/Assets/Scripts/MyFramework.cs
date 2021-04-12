1c86475f2cc5d16ddaea94af328bbb190ea23836
seonaOh <osa746@gmail.com>
2021-04-13 00:21:05 +0900

    메시생성 수정

--- dev-zed/assignment/Assets/Scripts/MyFramework.cs
+++ dev-zed/assignment/Assets/Scripts/MyFramework.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace UnityTemplateProjects
@@ -7,6 +8,7 @@ namespace UnityTemplateProjects
     public class MyFramework : MonoBehaviour
     {
         private APIResponseData m_data;
+        [SerializeField] private Texture m_texture;
 
         public void Start()
         {
@@ -33,15 +35,9 @@ namespace UnityTemplateProjects
 
                         List<Vector3> vectorList = new List<Vector3>();
                         for(int i = 0; i < floatArray.Length; i += 3)
-                        {
                             vectorList.Add(new Vector3(floatArray[i], floatArray[i + 2], floatArray[i + 1]));
 
-                            if(vectorList.Count >= 6)
-                            {
-                                CreateMesh(vectorList.ToArray());
-                                vectorList.Clear();
-                            }
-                        }
+                        CreateMesh(vectorList.ToArray());
                     }
                 }
             }
@@ -50,15 +46,45 @@ namespace UnityTemplateProjects
         private void CreateMesh(Vector3[] vectors)
         {
             var vertices = vectors;
-            int[] triangles = new int[] { 0, 2, 1, 1, 2, 3, 3, 2, 4, 4, 2, 5, 5, 2, 0 };
             Mesh mesh = new Mesh();
             mesh.vertices = vertices;
-            mesh.triangles = triangles;
+            List<int> triangles = new List<int>();
+            for(int i = 0; i < vectors.Length; i += 3)
+            {
+                triangles.Add(i);
+                triangles.Add(i + 1);
+                triangles.Add(i + 2);
+            }
+            mesh.triangles = triangles.ToArray();
 
             GameObject obj = new GameObject();
             obj.AddComponent<MeshFilter>();
             obj.AddComponent<MeshRenderer>();
+
             obj.GetComponent<MeshFilter>().mesh = mesh;
+
+            var material = new Material(Shader.Find("Unlit/Texture"));
+            material.SetTexture("_MainTex", m_texture);
+            material.SetTextureScale("_BaseMap", new Vector2(1f, 3));
+            obj.GetComponent<MeshRenderer>().material = material;
+
+            mesh.RecalculateBounds();
+            mesh.RecalculateNormals();
+
+            TextureMapping(ref mesh);
+        }
+
+        private void TextureMapping(ref Mesh mesh)
+        {
+            var meshVertices = mesh.vertices;
+            var normals = mesh.normals;
+            Vector2[] uvs = new Vector2[meshVertices.Length];
+            for(int i = 0; i < uvs.Length; i++)
+            {
+                uvs[i] = new Vector2(0, 0);
+            }
+
+            mesh.uv = uvs;
         }
 
         private APIResponseData LoadData()
